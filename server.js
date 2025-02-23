711b2edcb0b2f535d2249b2453cf61436b952c41
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-02-23 12:33:51 +0500

    working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-
+const socket = require("socket.io-client");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
